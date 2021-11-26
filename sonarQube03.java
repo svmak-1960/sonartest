5b576af14929c628bcc3865231abb421a1fea9d9
svmak-1960 <94986266+svmak-1960@users.noreply.github.com>
2021-11-26 18:55:21 +0100

    Update sonarQube03.java

--- sonarQube03.java
+++ sonarQube03.java
@@ -1,6 +1,7 @@
 public class sonarQube03 {
-    int calcNumbers(int start, int count){
+    int calcNumbers(int start){
         int accumulator = start;
+        int count = 10;
         while(count > 0){
             accumulator = accumulator + start;
         }
