234357413c16985ba9db97901056fe5c61ec9a0d
svmak-1960 <94986266+svmak-1960@users.noreply.github.com>
2021-11-26 18:07:58 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,24 @@
-package test;
 
-public class TestClass{
-  String addHeader(String stringToAdd){
-    return String.format("Added Header %s", stringToAdd);
-  }
+public class testClass{
+    private int x;
+    private int y;
+    
+    int getX() {
+        return x;
+    }
+    void setX(int val){
+        x = val;
+    }
+    
+    int getY() {
+        return y;
+    }
+    void setY(int val){
+        y = val;
+    }
+        
+    
+    String addHeader(String stringToAdd){
+        return String.format("Added Header %d %d %d", stringToAdd, x, y);
+    }
 }
