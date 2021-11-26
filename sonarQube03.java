35b343563ecfc12f522ddba98b163c33faeadc54
svmak-1960 <94986266+svmak-1960@users.noreply.github.com>
2021-11-26 18:46:52 +0100

    Delete sonarQube03.java

--- sonarQube03.java
+++
@@ -1,9 +0,0 @@
-public class sonarQube03 {
-    int calcNumbers(int start, int count){
-        int accumulator = start;
-        while(count > 0){
-            accumulator = accumulator + start;
-        }
-        return accumulator;
-    }
-}
