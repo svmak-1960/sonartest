9d3b76056e99f9d1ec082289e387e71ae4a51da9
svmak-1960 <94986266+svmak-1960@users.noreply.github.com>
2021-11-28 14:22:16 +0100

    Create sonarQube07.java

---
+++ sonarQube07.java
@@ -0,0 +1,10 @@
+public class sonarQube07 {
+    public int calcSum(int lastNum) {
+        int result = 0, j = 0, i;
+        for(i = 0; i < lastNum; j++){
+            i++;
+            result += j;
+        }
+        return result;
+    }
+}
