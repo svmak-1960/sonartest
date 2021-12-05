8a13a97f0414009f6faf33f0ab60eab3e45cb4fc
svmak-1960 <94986266+svmak-1960@users.noreply.github.com>
2021-12-05 11:58:57 +0100

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,24 +1,24 @@
 class A:
     @property
-    def foo(self, unexpected, unexpected2):  # Noncompliant. Too many parameters.
+    def foo(self):
         return self._foo
 
     @foo.setter
-    def foo(self, value, unexpected):  # Noncompliant. Too many parameters.
+    def foo(self, value):
         self._foo = value
 
     @foo.deleter
-    def foo(self, unexpected):  # Noncompliant. Too many parameters.
+    def foo(self):
         del self._foo
 
 class B:
-    def get_foo(self, unexpected):  # Noncompliant. Too many parameters.
+    def get_foo(self):
         return self._foo
 
-    def set_foo(self, value, unexpected):  # Noncompliant. Too many parameters.
+    def set_foo(self, value):
         self._foo = value
 
-    def del_foo(self, unexpected):  # Noncompliant. Too many parameters.
+    def del_foo(self):
         del self._foo
 
     foo = property(get_foo, set_foo, del_foo, "'foo' property.")
